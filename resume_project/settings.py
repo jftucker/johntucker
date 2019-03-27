74a2f20b6ee405acbe1a762219639436ec2db000
jftucker <johntucker0686@gmail.com>
2019-03-26 19:43:09 -0700

    reverting to mysqlite3

--- resume_project/settings.py
+++ resume_project/settings.py
@@ -94,22 +94,13 @@ WSGI_APPLICATION = 'resume_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-         'NAME': 'johntucker',
-         'USER': 'john',
-         'PASSWORD': 'Ihaveatinyhouse!1',
-         'HOST': 'localhost',
-         'PORT': '',
-     }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
